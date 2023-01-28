c65064a4520c9ec8d47ae9462517db845a853af2
dxlnr <daniel.illner@outlook.de>
2023-01-29 00:19:33 +0100

    bigram done.

--- bigram.py
+++ bigram.py
@@ -1,6 +1,7 @@
 """Various Language Models."""
 from typing import List 
 import numpy as np
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 
 def char_in_str(str: str):
@@ -12,7 +13,7 @@ def char_in_str(str: str):
     return clist
 
 
-def bigram(words: List):
+def bigram(words: List[str]):
     """Returns the Bigram of a list of str."""
     clist = char_in_str(''.join(words))
     bigram = np.zeros([len(clist), len(clist)])
@@ -21,11 +22,13 @@ def bigram(words: List):
         for c1, c2 in zip(w, w[1:]):
             bigram[clist.index(c1)][clist.index(c2)] += 1
 
-    return bigram / bigram.sum()
+    return bigram / bigram.sum(), clist
 
 if __name__ == "__main__":
     with open("names.txt", "r") as f:
         words = f.read().splitlines()
 
-        bgr = bigram(words)
-        print(bgr)
+        bgr, clist = bigram(words)
+
+        indices = np.where(bgr == bgr.max())
+        print(f"Highest combination of chars in names is: {clist[indices[0][0]]}{clist[indices[1][0]]}")
