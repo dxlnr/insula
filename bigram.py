9734afde3edb61f86a05933fcc69b4c13245456b
dxlnr <daniel.illner@outlook.de>
2023-01-29 01:50:38 +0100

    bigram small bug fix.

--- bigram.py
+++ bigram.py
@@ -1,34 +1,55 @@
 """Various Language Models."""
 from typing import List 
-import numpy as np
-np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+import torch
 
 
-def char_in_str(str: str):
-    clist = list()
-    for i in str:
-        if i not in clist:
-            clist.append(i)
-    clist.sort() 
-    return clist
+def def_lookup_table(words: List[str]):
+    """Returns a lookup table with all the chars in words plus special char 
+    for start and ending."""
+    clist = ['.'] + sorted(list(set(''.join(words)))) 
+    return {s:i for i, s in enumerate(clist)}
 
 
 def bigram(words: List[str]):
     """Returns the Bigram of a list of str."""
-    clist = char_in_str(''.join(words))
-    bigram = np.zeros([len(clist), len(clist)])
+    clt = def_lookup_table(words)
+    bigram = torch.zeros([len(clt), len(clt)], dtype=torch.int32)
 
     for w in words:
-        for c1, c2 in zip(w, w[1:]):
-            bigram[clist.index(c1)][clist.index(c2)] += 1
+        aw = ['.'] + list(w) + ['.']
+        for c1, c2 in zip(aw, aw[1:]):
+            bigram[clt[c1]][clt[c2]] += 1
 
-    return bigram / bigram.sum(), clist
+    return bigram
+
+
+def norm_bigram(bigram):
+    """Normalize the tensor along each row."""
+    bigram.float()
+    return bigram / bigram.sum(1, keepdim=True)
+
+
+def vis_bigram(bigram, words):
+    """Visualize Bigram via 2D plot."""
+    clist = ['.'] + sorted(list(set(''.join(words)))) 
+    lt = {i:s for i, s in enumerate(clist)}
+
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(16,16))
+    plt.imshow(bigram, cmap="Blues")
+
+    for i in range(bigram.shape[0]):
+        for j in range(bigram.shape[1]):
+            plt.text(j, i, lt[i] + lt[j], ha="center", va="bottom", color="gray")
+            plt.text(j, i, bigram[i, j].item(), ha="center", va="top", color="gray")
+    plt.axis('off')
+    plt.show()
+
 
 if __name__ == "__main__":
     with open("names.txt", "r") as f:
         words = f.read().splitlines()
-
-        bgr, clist = bigram(words)
-
-        indices = np.where(bgr == bgr.max())
-        print(f"Highest combination of chars in names is: {clist[indices[0][0]]}{clist[indices[1][0]]}")
+        # Compute the Bigram.
+        bgr = bigram(words)
+        # Visualize bigram matrix. 
+        vis_bigram(bgr, words)
