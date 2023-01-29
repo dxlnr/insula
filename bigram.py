3d93f0b3407d153829ed5288ccaad5a5a173b06b
dxlnr <daniel.illner@outlook.de>
2023-01-29 16:21:13 +0100

    constructing bigram via perceptron

--- bigram.py
+++ bigram.py
@@ -1,19 +1,21 @@
 """Various Language Models."""
-from sys import set_coroutine_origin_tracking_depth
-from typing import Dict, List 
+from typing import Dict, List
+
 import torch
+
+from data import create_dataset
 
 
 def def_lookup_table(words: List[str]):
-    """Returns a lookup table with all the chars in words plus special char 
+    """Returns a lookup table with all the chars in words plus special char
     for start and ending."""
-    clist = ['.'] + sorted(list(set(''.join(words)))) 
-    return {s:i for i, s in enumerate(clist)}
+    clist = ["."] + sorted(list(set("".join(words))))
+    return {s: i for i, s in enumerate(clist)}
 
 
 def invert_lookup(lt):
     """Flip keys & values."""
-    return {i:s for s, i in lt.items()}
+    return {i: s for s, i in lt.items()}
 
 
 def bigram(words: List[str], lt: Dict[str, int]) -> torch.Tensor:
@@ -25,7 +27,7 @@ def bigram(words: List[str], lt: Dict[str, int]) -> torch.Tensor:
     bigram = torch.zeros([len(lt), len(lt)], dtype=torch.int32)
 
     for w in words:
-        aw = ['.'] + list(w) + ['.']
+        aw = ["."] + list(w) + ["."]
         for c1, c2 in zip(aw, aw[1:]):
             bigram[lt[c1]][lt[c2]] += 1
 
@@ -43,7 +45,7 @@ def smoothing(bigram, s_v: int = 1):
     return bigram + s_v
 
 
-def sample_from_bigram(n_bigram, lt, seed: int = 132317238):
+def sample_from_bigram(n_bigram, lt, seed: int = 2329723):
     """Use the bigram params to extract new sample.
 
     :param bigram: Normalized 2D bigram matrix storing the params as probabilities.
@@ -54,12 +56,12 @@ def sample_from_bigram(n_bigram, lt, seed: int = 132317238):
     # Easier to use the inverted lookup table in this context.
     inv_lt = invert_lookup(lt)
 
-    sample = ''
+    sample = ""
     idx = 0
     while True:
         p = n_bigram[idx]
         idx = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-        
+
         # Append the chars based on lookup table.
         sample += inv_lt[idx]
         if idx == 0:
@@ -68,17 +70,39 @@ def sample_from_bigram(n_bigram, lt, seed: int = 132317238):
     return sample
 
 
-def nll(words, n_bigram, lt):
+def softmax(x):
+    """Softmax activation function."""
+    return x.exp() / x.exp().sum(1, keepdim=True)
+
+
+def nll(words: List[str], n_bigram: torch.Tensor, lt: Dict[str, int]):
     """Compute the negative log likelihood of some input words based on bigram."""
     log_l = 0.0
     n = 0
     for w in words:
-        aw = ['.'] + list(w) + ['.']
+        aw = ["."] + list(w) + ["."]
         for c1, c2 in zip(aw, aw[1:]):
             log_l += torch.log(n_bigram[lt[c1]][lt[c2]])
             n += 1
 
     return -log_l / n
+
+
+def train(xs, ys, w, lr: float = 0.1):
+    """Performing optimization."""
+    for i in range(50):
+        # Forward pass.
+        logits = torch.matmul(xs, w)
+        p = softmax(logits)
+        loss = -p[torch.arange(len(xs)), ys].log().mean()
+        print(f'loss: {loss.item():.3f}')
+
+        # Backward pass.
+        w.grad = None
+        loss.backward()
+        
+        # update
+        w.data += -lr * w.grad
 
 
 def vis_bigram(bigram, lt):
@@ -87,14 +111,15 @@ def vis_bigram(bigram, lt):
     inv_lt = invert_lookup(lt)
 
     import matplotlib.pyplot as plt
-    plt.figure(figsize=(16,16))
+
+    plt.figure(figsize=(16, 16))
     plt.imshow(bigram, cmap="Blues")
 
     for i in range(bigram.shape[0]):
         for j in range(bigram.shape[1]):
             plt.text(j, i, lt[i] + lt[j], ha="center", va="bottom", color="gray")
             plt.text(j, i, bigram[i, j].item(), ha="center", va="top", color="gray")
-    plt.axis('off')
+    plt.axis("off")
     plt.show()
 
 
@@ -106,16 +131,19 @@ if __name__ == "__main__":
         # Compute the Bigram.
         bgr = bigram(words, lt)
 
-        # Visualize bigram matrix. 
+        # Visualize bigram matrix.
         # vis_bigram(bgr, words)
 
         # Normalize the bigram along the rows.
         n_bgr = norm_bigram(bgr)
 
         # Draw a new sample from bigram.
-        set_of_words = [sample_from_bigram(n_bgr, lt, seed=i) for i in range(10)]
+        # set_of_words = [sample_from_bigram(n_bgr, lt, seed=i) for i in range(10)]
 
-        print(set_of_words)
-
-        nll = nll(['andrejq'], n_bgr, lt)
-        print(f"nll = {nll:.3f}")
+        # Create training data.
+        xs, ys = create_dataset(words, lt)
+        # Input weights.
+        w = torch.randn((len(lt), len(lt)), requires_grad=True)
+        
+        # Perform training.
+        train(xs, ys, w, lr=50)
---
+++ data.py
@@ -0,0 +1,19 @@
+"""Dataset"""
+import torch
+
+
+def create_dataset(words, lt):
+    """Prepares a training dataset from list of words."""
+    xs, ys = list(), list()
+
+    for w in words:
+        aw = ["."] + list(w) + ["."]
+        for c1, c2 in zip(aw, aw[1:]):
+            xs.append(lt[c1])
+            ys.append(lt[c2])
+
+    xs, ys = torch.tensor(xs), torch.tensor(ys)
+
+    return (
+        torch.nn.functional.one_hot(xs, len(lt)).float(), ys
+    )
